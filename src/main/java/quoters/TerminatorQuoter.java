d59426bbf347d3e2593b40df8a90318f8d215184
Igor Shangin <igorsan1@mail.ru>
2020-05-31 18:51:03 +0300

    Add ApplicationListener

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -18,11 +18,14 @@ public class TerminatorQuoter implements Quoter {
         System.out.println("Phase 2");
         System.out.println(repeat);
     }
+
     public void setMessage(String message) {
         this.message = message;
     }
 
+    @PostProxy
     public void sayQuote() {
+        System.out.println("Phase 3");
         for (int i = 0; i < repeat; i++) {
             System.out.println(message);
         }
