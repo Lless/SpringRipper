46e83c1006523f3be5f51b39f4e82b5edcf63319
Igor Shangin <igorsan1@mail.ru>
2020-06-01 16:51:56 +0300

    Add BeanFactoryPostProcessor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import quoters.Quoter;
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(Quoter.class).sayQuote();
     }
 }
